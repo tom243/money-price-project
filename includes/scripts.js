ec1bbd0c14f45bbe1553a6c18339f88b817772f9
Roni6ch <ronny.kabra@gmail.com>
2015-01-28 22:33:40 +0200

    sound

--- includes/scripts.js
+++ includes/scripts.js
@@ -16,11 +16,11 @@ window.onload = function() {
 };
 
 var fruitsAndVedgArray = new Array("images/tapuah.png", "images/tapuah yonatan.png",  "images/banana.png", "images/tapuz shmuti.png", "images/tapuz valensiya.png", "images/klemantina.png", "images/eshkolit.png", "images/agvaniya.png", "images/melafefon.png", "images/pilpel yarok.png", "images/pilpel yarok bahir.png", "images/pilpel yarok kehe.png", "images/shauit yaruka.png", "images/agas.png", "images/afarsek.png", "images/mishmesh.png", "images/shezif sagol.png", "images/shezif zahov.png", "images/anavom shorim.png", "images/anavim yarukim.png", "images/kruvit.png", "images/hazil.png", "images/gezer.png", "images/tapuah adama.png", "images/tiras.png", "images/mango.png", "images/avokado.png", "images/melon galya.png", "images/avatiah.png", "images/afarsemon.png", "images/limon.png", "images/bazal.png", "images/hasa.png", "images/kruv lavan.png", "images/agvaniot shery.png", "images/znon.png");
-var milkArray = new Array("images/tapuz valensiya.png","images/halav be sakit.png", "images/yogurt tevi.png", "images/shamenet hamuza.png", "images/koteg.png", "images/hema.png", "images/leben.png", "images/gvina levana.png", "images/bezim medium.png", "images/bezim larj.png");
+var milkArray = new Array("images/halav be sakit.png", "images/yogurt tevi.png", "images/shamenet hamuza.png", "images/tahlif halav.png","images/koteg.png", "images/hema.png", "images/leben.png", "images/gvina levana.png", "images/bezim medium.png", "images/bezim larj.png");
 var CannedFoodArray = new Array("images/afunat gina.png", "images/tiras.png", "images/resek sgvaniot.png", "images/salat hazilim.png", "images/salat humus.png", "images/zeitim be mishkal.png");
 var meatArray = new Array("images/haze of.png", "images/karpion.png", "images/file nesihat ha nilus.png", "images/pastrama.png", "images/basar bakar haze.png", "images/basar bakar zlaot.png", "images/naknikiot.png", "images/of tari.png", "images/of kafu.png");
 var breadArray = new Array("images/hala.png", "images/biskuitim.png", "images/itriot.png", "images/orez ragil.png", "images/orez male.png", "images/kornfleks.png", "images/spagety.png", "images/leham lavan ahid.png", "images/lehem ahid kehe.png");
-var oilArray = new Array("images/shemen soya.png", "images/margarina.png", "images/margarina le mericha.png", "images/shemen zait.png", "images/tahlif halav.png", "images/te.png", "images/kafe shahor.png", "images/kafe names.png", "images/avkat kakao.png", "images/sugar.png", "images/riba.png", "images/konfetura.png", "images/sukariot toffi.png", "images/shokolad.png");
+var oilArray = new Array("images/shemen soya.png", "images/margarina.png", "images/margarina le mericha.png", "images/shemen zait.png",  "images/te.png", "images/kafe shahor.png", "images/kafe names.png", "images/avkat kakao.png", "images/sugar.png", "images/riba.png", "images/konfetura.png", "images/sukariot toffi.png", "images/shokolad.png");
 var drinksArray = new Array("images/bira levana.png", "images/mashke mugaz.png", "images/mashke pri hadar.png", "images/brendy.png");
 
 function Img(imgSrc, id) {
@@ -61,14 +61,6 @@ function Img(imgSrc, id) {
 				foodArray[linkObj.getAttribute('id')].count += 1;
 				$("input[type='textbox'][id='" + idObj + "']").val(foodArray[linkObj.getAttribute('id')].count);
 			});
-			
-			//number
-			$('<input />', {
-				type : 'textbox',
-				id : linkObj.getAttribute('id'),
-				val : foodArray[linkObj.getAttribute('id')].count
-			}).appendTo($($x));
-			
 			//minus
 			$('<input />', {
 				type : 'button',
@@ -81,6 +73,15 @@ function Img(imgSrc, id) {
 					$("input[type='textbox'][id='" + idObj + "']").val(foodArray[linkObj.getAttribute('id')].count);
 				}
 			});
+			
+			//number
+			$('<input />', {
+				type : 'textbox',
+				id : linkObj.getAttribute('id'),
+				val : foodArray[linkObj.getAttribute('id')].count
+			}).appendTo($($x));
+			
+			
 			//product name
 			$.getJSON("jsons/objects.json", function(data) {
 				$.each(data, function(key, val) {
