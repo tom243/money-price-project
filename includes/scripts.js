84b6a5a4a42dd380c8c306375f764c7a0ecaff12
Roni6ch <ronny.kabra@gmail.com>
2015-02-06 16:07:55 +0200

    d3

--- includes/scripts.js
+++ includes/scripts.js
@@ -121,18 +121,93 @@ function ImgsManager(groceryArray, id) {
 	}
 };
 
+///////////////////////////////////////////////////////// D3 /////////////////////////////////////////////////////////
 
-/*
- $(document).ready(function() {
- var width = $(document).width();
- $('footer').css('width', width + 'px');
- });
+var salaryArray = [];
+var yearsArray = [];
+var data = [];
+var k;
+$(document).ready(function() {
+	$.getJSON("jsons/salary.json", function(data) {
+		$.each(data, function(key, val) {
+			$.each(val, function(key2, val2) {
+				salaryArray.push(val2);
+				//salary each year
+				yearsArray.push(parseInt(key));
+				//years 1986-2013
+			});
+		});
+		readyJson();
+	});
+});
 
- $(document).ready(function() {
- function autoResizeDiv()
- {
- $('main').css('height',window.innerHeight + 'px');
- }
- window.onresize = autoResizeDiv;
- autoResizeDiv();
- });*/
+function readyJson() {
+	var margin = {
+		top : 20,
+		right : 20,
+		bottom : 30,
+		left : 50
+	}, width = 1350 - margin.left - margin.right, height = 480 - margin.top - margin.bottom;
+
+	var parseDate = d3.time.format("%d-%b-%y").parse;
+
+	var x = d3.scale.linear().range([0, width]);
+
+	var y = d3.scale.linear().range([height, 0]);
+
+	var xAxis = d3.svg.axis().scale(x).orient("bottom");
+
+	var yAxis = d3.svg.axis().scale(y).orient("left");
+
+	var line = d3.svg.line().x(function(d) {
+		return x(d[0]);
+	}).y(function(d) {
+		return y(d[1]);
+	});
+	var svg = d3.select("#d3").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	var yearsArrayLength = yearsArray.length;
+
+	var i;
+	for ( i = yearsArrayLength - 1; i >= 0; i--) {
+		data.push([yearsArray[i], salaryArray[i]]);
+	}
+
+	x.domain(d3.extent(data, function(d) {
+		return d[0];
+	}));
+	y.domain(d3.extent(data, function(d) {
+		return d[1];
+	}));
+
+	svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
+
+	svg.append("g").attr("class", "y axis").call(yAxis).append("text").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text("Price ($)");
+
+	svg.append("path").datum(data).attr("class", "line").attr("d", line);
+
+	svg.selectAll(".point").data(data).enter().append("svg:circle").attr("class", "point").attr("cx", function(d) {
+		return x(d[0]);
+	}).attr("cy", function(d) {
+		return y(d[1]);
+	});
+
+	var selected_year = 2013;
+	function set_radius() {
+		svg.selectAll(".point").data(data).attr("r", function(d) {
+			if (d[0] != selected_year) {
+				return 14.3;
+			} else {
+				return 27.7;
+			}
+		});
+	}
+
+
+	$('body').on('click', function() {
+		console.log('click');
+		selected_year -= 1;
+		set_radius();
+	});
+
+	set_radius();
+}
